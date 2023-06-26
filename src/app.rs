a900db1e02c97ee634a50acfa5267abda8745eaa
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2023-06-26 22:22:43 +0530

    move config verification to separate file

--- src/app.rs
+++ src/app.rs
@@ -14,11 +14,7 @@ scaffolder config folder
 
 */
 
-use std::fs;
-
-use directories::ProjectDirs;
-
-use crate::types::ScaffoldArgs;
+use crate::{types::ScaffoldArgs, verify_config::VerifyConfig};
 
 #[derive(Debug)]
 pub struct App {
@@ -31,29 +27,6 @@ impl App {
 
         Ok(App { args })
     }
-
-    fn verify_args(args: &mut ScaffoldArgs) -> Result<(), Box<dyn std::error::Error>> {
-        let config_dir = match args.config {
-            Some(ref config) => config.clone(),
-            None => match ProjectDirs::from("com", "TheIcyColdPenguin", "scaffolder") {
-                Some(proj_dirs) => proj_dirs.config_dir().to_path_buf(),
-                None => return Err("Couldn't create or find config directory".into()),
-            },
-        };
-
-        fs::create_dir_all(config_dir.join("templates"))?;
-        fs::create_dir_all(config_dir.join("premade"))?;
-
-        let config_file = fs::File::options()
-            .read(true)
-            .create(true)
-            .append(true)
-            .open(config_dir.join("scaffolder.toml"));
-
-        dbg!(&config_file);
-
-        args.config = Some(config_dir);
-
-        Ok(())
-    }
 }
+
+impl VerifyConfig for App {}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use types::ScaffoldArgs;
 
 mod app;
 mod types;
+mod verify_config;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = ScaffoldArgs::parse();
